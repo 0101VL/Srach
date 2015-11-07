80597e28251d27dcd8630db7d7a8ba0af9ffd521
maksimbezgodov <maksim_bezgodov@mail.ru>
2015-11-07 19:28:52 +1000

    first files

---
+++ registration.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Регистрация</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form method="post">
+			E-mail: <input type="text" name="email"><br>
+			Пароль: <input type="password" name="password"><br>
+			Подтверждение пароля: <input type="password" name="password"><br>
+			<input type="submit" name="submit" value="Отправить"><br>
+		</form>
+	</body>
+</html>
